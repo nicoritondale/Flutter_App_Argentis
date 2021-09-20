57fad63c29d6dbf90dcb09c0e8bba3ae9100ccd3
nicoritondale <hritondale@gmail.com>
2021-09-20 15:40:09 -0400

    init

---
+++ lib/src/home_temp.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(children: _crearItemsCorta()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Components'),
+            // El encargado de poner icono o otra cosa a la izquierda
+            leading: Icon(Icons.account_balance_wallet),
+            // Y este a la derecha
+            trailing: Icon(Icons.keyboard_arrow_right),
+            // Al hacer un click que haga esto
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
